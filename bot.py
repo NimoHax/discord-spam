0d1f9f67d5123c413e8e897f8044e3a099cc96da
Noob Gamer <39646826+biswajit9123@users.noreply.github.com>
2019-01-09 15:56:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,16 +11,18 @@ async def on_ready():
     print(client.user.name)
 
 guildids = ["519451035836874763"]
-channelids = ["519490720583778307", "519490720583778307", "520494704442212382"]
+channelids = ["519490720583778307", "520494704442212382", "519491305324281857"]
 
 @client.event
 async def on_message(message):
+    if message.author.bot:
+        return
     if message.channel.id not in channelids and message.server.id in guildids:
         def check(message):
-            return message
-        spam = await client.wait_for_message(author=message.author, channel= message.channel,check=check, timeout=2)
-        spam1 = await client.wait_for_message(author=message.author, check=check, timeout=2, channel= message.channel)
-        spam2 = await client.wait_for_message(author=message.author, check=check, timeout=2, channel= message.channel)
+            return message.content
+        spam = await client.wait_for_message(author=message.author, channel= message.channel,check=check, timeout=1)
+        spam1 = await client.wait_for_message(author=message.author, check=check, timeout=1, channel= message.channel)
+        spam2 = await client.wait_for_message(author=message.author, check=check, timeout=1, channel= message.channel)
         await client.delete_message(spam)
         await client.delete_message(spam1)
         await client.delete_message(spam2)
